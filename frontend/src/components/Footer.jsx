ec4b0c9a9e7d5592a9a02b9762a3a2640a38d9d4
Philip Simpson <dev.phits@gmail.com>
2023-01-24 06:35:36 -0800

    React-bootstrap setup, Header & Footer Components

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import { Container, Row, Col } from 'react-bootstrap'
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <Col className='text-center py-3'>Copyright &copy; SimpsonSoftware 2023</Col>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
