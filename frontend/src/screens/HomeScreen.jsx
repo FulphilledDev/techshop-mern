a3a89f9c853fb001f9934f2bef667c6baeeaa19a
Philip Simpson <dev.phits@gmail.com>
2023-01-28 12:40:28 -0800

    Bring redux into HomeScreen w/ useDispatch and useSelector

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,31 +1,37 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'
+import { getProductList } from '../features/products/productSlice'
 
 const HomeScreen = () => {
-  const [ products, setProducts ] = useState([])
+  const dispatch = useDispatch()
+  
+  const productList = useSelector((state) => state.productList)
+
+  const { isLoading, isError, message, products } = productList
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data }= await axios.get('/products')
+    dispatch(getProductList())
+  }, [dispatch])
 
-      setProducts(data)
-    }
-
-    fetchProducts()
-  }, [])
 
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
-            </Col>
-        ))}
-      </Row>
+      {isLoading 
+        ? <h2>Loading...</h2> 
+        : isError ? <h3>{message}</h3> 
+        : (
+          <Row>
+            {products.map((product) => (
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                    <Product product={product} />
+                </Col>
+            ))}
+          </Row>
+        )}
+      
     </>
   )
 }
